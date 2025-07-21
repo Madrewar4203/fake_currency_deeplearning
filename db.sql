399b4a0ca88841ecaa4d49d5cba43d4fe80c9ff6
Madrewar4203 <abmadrewar423@gmail.com>
2025-07-21 12:34:01 +0530

    Initial commit

---
+++ db.sql
@@ -0,0 +1,9 @@
+drop database if exists fake_indian_currency;
+create database fake_indian_currency;
+use fake_indian_currency;
+
+create table users (
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    email VARCHAR(50), 
+    password VARCHAR(50)
+    );
